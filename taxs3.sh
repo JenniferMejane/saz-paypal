5d5d6b281b8a767e8d276474fed60e074e05ffc1
jennifer K.M <jennifermejane@gmail.com>
2023-08-15 05:37:49 -0700

    my first calculator

---
+++ taxs3.sh
@@ -0,0 +1,13 @@
+#my calculator
+echo "pls enter your choice (-,+,/,%,\*)"
+echo "enter first number"
+read num1
+echo "enter choice"
+read choice
+echo "enter ssecond num"
+read num2
+echo "$num1 $choice $num2"
+echo "your answer is:"
+expr $num1 $choice $num2
+
+
